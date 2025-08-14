d6f5c2492f56ff37a5bffb9fd984a26568d1984d
Agustin Petrini <agustin@sid-8.com>
2025-08-14 16:43:49 +0900

    Implement basic param processing

--- src/main.c
+++ src/main.c
@@ -7,9 +7,47 @@
 #include "file.h"
 #include "parse.h"
 
-void print_usage(char *argv[]) {
+void print_usage(char *argv[])
+{
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t -n  create new database file\n");
+  printf("\t -f  (required) path to database file\n");
 }
 
-int main(int argc, char *argv[]) { 
-	
+int main(int argc, char *argv[])
+{
+  bool new_file = false;
+  char *filepath = NULL;
+  int c = 0;
+
+  while((c = getopt(argc, argv, "nf:")) != -1)
+  {
+    switch (c)
+    {
+      case 'n':
+        new_file = true;
+        break;
+      case 'f':
+        filepath = optarg;
+        break;
+      case '?':
+        print_usage(argv);
+        return -1;
+      default:
+        return -1;
+    }
+  }
+
+  if(filepath == NULL)
+  {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
+
+    return -1;
+  }
+
+  printf("new_file: %d\n", new_file);
+  printf("filepath: %s\n", filepath);
+
+  return 0;
 }
