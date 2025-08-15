db969972dcc2e18626e5e47c744eb8d02d2563ea
Agustin Petrini <agustin@sid-8.com>
2025-08-15 12:01:50 +0900

    Add null ref check

--- src/parse.c
+++ src/parse.c
@@ -101,6 +101,12 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 }
 
 int create_db_header(struct dbheader_t **headerOut) {
+  if(headerOut == NULL)
+  {
+    printf("Illegal header pointer\n");
+    return STATUS_ERROR;
+  }
+
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
 	if (header == NULL) {
 		printf("Malloc failed to create db header\n");
